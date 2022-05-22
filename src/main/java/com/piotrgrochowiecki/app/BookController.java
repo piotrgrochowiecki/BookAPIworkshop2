c9605c370e9b94b648620ebb998617d593a8dce2
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-05-22 17:42:20 +0200

    Changed configuration and initialization

--- src/main/java/com/piotrgrochowiecki/app/BookController.java
+++ src/main/java/com/piotrgrochowiecki/app/BookController.java
@@ -1,14 +1,13 @@
 package com.piotrgrochowiecki.app;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("books")
 public class BookController {
 
-    @RequestMapping("/helloBook")
+    @RequestMapping("helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
